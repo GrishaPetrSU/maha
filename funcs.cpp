dd1b84f54dc2139b445552d379dda039b5e8bc0e
Grish Edigarev <edigarev.griha@gmail.com>
2018-12-06 16:07:02 +0300

    func Mul

---
+++ funcs.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h" #include "funcs.h"
+double MySum(double a, double b) 
+{ return a + b; }
+double MySub(double a, double b) 
+{ return a - b; }
+double MyMul(double a, double b) 
+{ return a * b; }
